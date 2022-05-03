1c1e821cc68cff2393a393c9f64fd232d90e02f8
sanchez.101 <sanchez.101@mail.ru>
2022-05-03 20:57:04 +0300

    Add post

---
+++ HubOfChess.Domain/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace HubOfChess.Domain
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public User Author { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
