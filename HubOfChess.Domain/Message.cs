f0e9165d448ad5bd900f30d33580b254de0d6fac
sanchez.101 <sanchez.101@mail.ru>
2022-05-03 20:54:01 +0300

    Add message

---
+++ HubOfChess.Domain/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace HubOfChess.Domain
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Chat Chat { get; set; }
+        public User Sender { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
