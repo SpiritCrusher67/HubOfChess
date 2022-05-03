710a5d6394e92da879179961426b8e1e8b93b012
sanchez.101 <sanchez.101@mail.ru>
2022-05-03 20:49:24 +0300

    Add user

---
+++ HubOfChess.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HubOfChess.Domain
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AboutMe { get; set; }
+        public int XPCount { get; set; }
+        public IEnumerable<User> Friends { get; set; }
+
+    }
+}
