09d151565c6efd472cf0c96d7f328d7deba3575a
sanchez.101 <sanchez.101@mail.ru>
2022-05-03 21:01:10 +0300

    Add post like and comment

--- HubOfChess.Domain/Post.cs
+++ HubOfChess.Domain/Post.cs
@@ -7,6 +7,20 @@
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+    }
 
+    public class PostLike
+    {
+        public Post Post{ get; set; }
+        public User User { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public class PostComment
+    {
+        public Post Post { get; set; }
+        public User User { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
     }
 }
