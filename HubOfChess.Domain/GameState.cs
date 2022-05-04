9f76afe44bde10a09e1ebaf97310b2cb8b64f39e
sanchez.101 <sanchez.101@mail.ru>
2022-05-04 14:40:45 +0300

    Add post config

--- HubOfChess.Domain/GameState.cs
+++ HubOfChess.Domain/GameState.cs
@@ -1,10 +1,10 @@
 ﻿namespace HubOfChess.Domain
 {
-    public class Game
+    public class GameState
     {
         public Guid Id { get; set; }
-        public User WhiteSideUser { get; set; }
-        public User BlackSideUser { get; set; }
+        public User? WinnerUser { get; set; }
+        public IEnumerable<User> Users { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public string? MovesHistory { get; set; }
