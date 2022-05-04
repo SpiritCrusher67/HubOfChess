195c096cd39cbfe8159d32c3f84be36673547b3c
sanchez.101 <sanchez.101@mail.ru>
2022-05-04 23:14:53 +0300

    Add chat owner and name is now nullable

--- HubOfChess.Domain/Chat.cs
+++ HubOfChess.Domain/Chat.cs
@@ -3,7 +3,8 @@
     public class Chat
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        public User? Owner { get; set; }
         public IEnumerable<User> Users { get; set; }
     }
 }
