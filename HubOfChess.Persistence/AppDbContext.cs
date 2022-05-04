8317e57a47169e3cc5c3d92b17b0134e914538fe
sanchez.101 <sanchez.101@mail.ru>
2022-05-04 16:04:06 +0300

    Add IAppDbContext implementation on Persistence layer

---
+++ HubOfChess.Persistence/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using HubOfChess.Application.Interfaces;
+using HubOfChess.Domain;
+using HubOfChess.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Persistence
+{
+    public sealed class AppDbContext : DbContext, IAppDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostLike> PostLikes { get; set; }
+        public DbSet<PostComment> PostComments { get; set; }
+        public DbSet<GameState> GameStates { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ChatConfiguration());
+            modelBuilder.ApplyConfiguration(new MessageConfiguration());
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
+            modelBuilder.ApplyConfiguration(new PostLikeConfiguration());
+            modelBuilder.ApplyConfiguration(new PostCommentConfiguration());
+            modelBuilder.ApplyConfiguration(new GameStateConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
