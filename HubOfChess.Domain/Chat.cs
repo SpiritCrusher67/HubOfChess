bd6da171a897da93c65551cf31b4b7c1c96abb86
sanchez.101 <sanchez.101@mail.ru>
2022-05-04 23:26:31 +0300

    Update Chat model. Add collection for Message.

--- HubOfChess.Domain/Chat.cs
+++ HubOfChess.Domain/Chat.cs
@@ -6,5 +6,6 @@
         public string? Name { get; set; }
         public User? Owner { get; set; }
         public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Message> Messages { get; set; }
     }
 }
