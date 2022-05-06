e315ead13d192b78c6229cba327c960f99544a9b
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 21:26:03 +0300

    Add GetChatById quert, handler and tests

---
+++ HubOfChess.Application/Chats/Queries/GetChatById/GetChatByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HubOfChess.Domain;
+using MediatR;
+
+namespace HubOfChess.Application.Chats.Queries.GetChatById
+{
+    public record GetChatByIdQuery(Guid ChatId, Guid UserId) : IRequest<Chat>;
+}
---
+++ HubOfChess.Tests/Chats/Queries/GetChatByIdQueryHandlerTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HubOfChess.Application.Chats.Queries.GetChatById;
+using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Tests.Common;
+using Xunit;
+
+namespace HubOfChess.Tests.Chats.Queries
+{
+    public class GetChatByIdQueryHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task GetChatByIdQueryTest_Success()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserA.UserId;
+            var chatId = AppDbContextFactory.ChatA.Id;
+            var handler = new GetChatByIdQueryHandler(DbContext);
+
+            //Act
+            var chat = await handler.Handle(
+                new GetChatByIdQuery(chatId,userId),
+                CancellationToken.None);
+
+            //Assert
+            Assert.NotNull(chat);
+            Assert.Equal(chatId, chat.Id);
+            Assert.Equal(AppDbContextFactory.ChatA, chat);
+        }
+
+        [Fact]
+        public async Task GetChatByIdQueryTest_FailNotExistingChat()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserA.UserId;
+            var chatId = Guid.NewGuid();
+            var handler = new GetChatByIdQueryHandler(DbContext);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>( () =>
+                handler.Handle(
+                new GetChatByIdQuery(chatId, userId),
+                CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task GetChatByIdQueryTest_FailNotExistingUser()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var chatId = AppDbContextFactory.ChatA.Id;
+            var handler = new GetChatByIdQueryHandler(DbContext);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+               handler.Handle(
+               new GetChatByIdQuery(chatId, userId),
+               CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task GetChatByIdQueryTest_FailOnWrongUserId()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserC.UserId;
+            var chatId = AppDbContextFactory.ChatA.Id;
+            var handler = new GetChatByIdQueryHandler(DbContext);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NoPermissionException>(() =>
+               handler.Handle(
+               new GetChatByIdQuery(chatId, userId),
+               CancellationToken.None));
+        }
+    }
+}
