ccb4cfef4dc81c39c94e60f98f22923612df65f4
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 17:30:47 +0300

    Add new entity UserFriend for relationship many to many. Add Prop OwnedChats

--- HubOfChess.Domain/User.cs
+++ HubOfChess.Domain/User.cs
@@ -6,10 +6,11 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
-        public string AboutMe { get; set; }
+        public string? AboutMe { get; set; }
         public int XPCount { get; set; }
-        public IEnumerable<User> Friends { get; set; }
+        public IEnumerable<UserFriend> Friends { get; set; }
         public IEnumerable<Chat> Chats { get; set; }
+        public IEnumerable<Chat> OwnedChats { get; set; }
         public IEnumerable<Message> Messages { get; set; }
         public IEnumerable<Post> Posts { get; set; }
         public IEnumerable<PostLike> PostLikes { get; set; }
@@ -17,5 +18,12 @@
         public IEnumerable<GameState> GamesPlayed { get; set; }
         public IEnumerable<GameState> WinningGames { get; set; }
 
+    }
+    public class UserFriend
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid FriendId { get; set; }
+        public User Friend { get; set; }
     }
 }
