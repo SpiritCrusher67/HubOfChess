dc6e3b874b402ec7085328b099c3cbfee7e768e7
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 17:49:01 +0300

    Add chat creating command, handler and tests.

---
+++ HubOfChess.Tests/Chats/Commands/CreateChatCommandHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using HubOfChess.Application.Chats.Commands.CreateChat;
+using HubOfChess.Tests.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HubOfChess.Tests.Chats.Commands
+{
+    public class CreateChatCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task CreateChatCommand_Success()
+        {
+            //Arrange
+            var user = DbContext.Users.First();
+            var name = "84466A55";
+            var handler = new CreateChatCommandHandler(DbContext);
+
+            //Act
+            var chatId = await handler.Handle(new CreateChatCommand(user, name), CancellationToken.None);
+
+            //Assert
+            Assert.NotNull(await DbContext.Chats.SingleOrDefaultAsync(c => c.Id == chatId));
+        }
+    }
+}
