96bd732d31e32673e54b5ac3a53132f111d7f020
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 18:27:53 +0300

    Bug fix in test db context

--- HubOfChess.Tests/Common/AppDbContextFactory.cs
+++ HubOfChess.Tests/Common/AppDbContextFactory.cs
@@ -8,13 +8,42 @@ namespace HubOfChess.Tests.Common
 {
     public static class AppDbContextFactory
     {
-        public static readonly User UserA = new() { UserId = Guid.NewGuid() };
-        public static readonly User UserB = new() { UserId = Guid.NewGuid() };
-        public static readonly User UserC = new() { UserId = Guid.NewGuid() };
+        public static readonly User UserA = new() { UserId = Guid.NewGuid(), FirstName = "User", LastName = "A" };
+        public static readonly User UserB = new() { UserId = Guid.NewGuid(), FirstName = "User", LastName = "B" };
+        public static readonly User UserC = new() { UserId = Guid.NewGuid(), FirstName = "User", LastName = "C" };
 
-        public static readonly Chat ChatA = new() { Id = Guid.NewGuid(), Users = new List<User> { UserA, UserB } };
-        public static readonly Chat ChatB = new() { Id = Guid.NewGuid(), Users = new List<User> { UserA, UserC } };
+        public static readonly Chat ChatA = new() { Id = Guid.NewGuid(), Users = new List<User> { UserA, UserB }, Owner = UserA };
+        public static readonly Chat ChatB = new() { Id = Guid.NewGuid(), Users = new List<User> { UserA, UserC }, Owner = UserC };
         public static readonly Chat ChatC = new() { Id = Guid.NewGuid(), Users = new List<User> { UserA, UserB, UserC }, Owner = UserA, Name = "TestChat" };
+
+        public static readonly Message MessageA = new()
+        {
+            Id = Guid.Parse("DA0EF73A-BB52-4E44-B24F-F241418FFA59"),
+            Chat = ChatA,
+            Sender = UserA,
+            Text = "Test msg A"
+        };
+        public static readonly Message MessageB = new()
+        {
+            Id = Guid.Parse("112E2AD7-A9EE-4E6A-8FCC-A532A5EE846C"),
+            Chat = ChatA,
+            Sender = UserB,
+            Text = "Test msg B"
+        };
+        public static readonly Message MessageC = new()
+        {
+            Id = Guid.Parse("3BB8D12F-C17A-42EB-B945-315559BB65E0"),
+            Chat = ChatC,
+            Sender = UserA,
+            Text = "Test msg C"
+        };
+        public static readonly Message MessageD = new()
+        {
+            Id = Guid.Parse("7B301DDA-DEBA-433B-AE11-13380142B0BF"),
+            Chat = ChatC,
+            Sender = UserB,
+            Text = "Test msg D"
+        };
 
         public static AppDbContext Create()
         {
@@ -27,30 +56,7 @@ namespace HubOfChess.Tests.Common
             context.Users.AddRange(UserA, UserB, UserC);
             context.Chats.AddRange(ChatA, ChatB, ChatC);
             context.Messages.AddRange(
-                new Message() 
-                    { 
-                        Id = Guid.Parse("DA0EF73A-BB52-4E44-B24F-F241418FFA59"),
-                        Chat = ChatA,
-                        Sender = UserA
-                    },
-                new Message()
-                {
-                    Id = Guid.Parse("112E2AD7-A9EE-4E6A-8FCC-A532A5EE846C"),
-                    Chat = ChatA,
-                    Sender = UserB
-                },
-                new Message()
-                {
-                    Id = Guid.Parse("3BB8D12F-C17A-42EB-B945-315559BB65E0"),
-                    Chat = ChatC,
-                    Sender = UserA
-                },
-                new Message()
-                {
-                    Id = Guid.Parse("7B301DDA-DEBA-433B-AE11-13380142B0BF"),
-                    Chat = ChatC,
-                    Sender = UserB
-                }
+                MessageA, MessageB, MessageC,MessageD
             );
 
             context.SaveChanges();
