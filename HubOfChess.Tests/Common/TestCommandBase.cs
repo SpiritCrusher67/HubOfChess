bd305e013796c322d1e213f03f3e3b1bb804eac5
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 17:47:54 +0300

    Add base class for command tests

---
+++ HubOfChess.Tests/Common/TestCommandBase.cs
@@ -0,0 +1,20 @@
+﻿using HubOfChess.Persistence;
+using System;
+
+namespace HubOfChess.Tests.Common
+{
+    public abstract class TestCommandBase : IDisposable
+    {
+        protected readonly AppDbContext DbContext;
+
+        public TestCommandBase()
+        {
+            DbContext = AppDbContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            AppDbContextFactory.Destroy(DbContext);
+        }
+    }
+}
