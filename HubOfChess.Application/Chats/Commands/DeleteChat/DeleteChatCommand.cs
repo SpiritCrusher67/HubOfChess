6c5f33f9d8d42e8e0da43b6c512929c05e78dc75
sanchez.101 <sanchez.101@mail.ru>
2022-05-06 17:49:40 +0300

    Add delete chat command, behavior and tests

---
+++ HubOfChess.Application/Chats/Commands/DeleteChat/DeleteChatCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace HubOfChess.Application.Chats.Commands.DeleteChat
+{
+    public record DeleteChatCommand(Guid ChatId, Guid? UserId) : IRequest;
+}
---
+++ HubOfChess.Application/Chats/Commands/DeleteChat/DeleteChatCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using HubOfChess.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using HubOfChess.Domain;
+using HubOfChess.Application.Common.Exceptions;
+
+namespace HubOfChess.Application.Chats.Commands.DeleteChat
+{
+    public class DeleteChatCommandHandler : IRequestHandler<DeleteChatCommand>
+    {
+        private readonly IAppDbContext _dbContext;
+
+        public DeleteChatCommandHandler(IAppDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(DeleteChatCommand request, CancellationToken cancellationToken)
+        {
+            var chat = await _dbContext.Chats
+                .FirstOrDefaultAsync(c => c.Id == request.ChatId);
+
+            if (chat == null || chat.Owner?.UserId != request.UserId)
+                throw new NotFoundException(nameof(Chat), request.ChatId);
+
+            _dbContext.Chats.Remove(chat);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
