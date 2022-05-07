8735a671ee97df3717e604c012983eb3f2699c6b
sanchez.101 <sanchez.101@mail.ru>
2022-05-07 16:48:26 +0300

    Add GetMessages By ChatId Query, Handler and Tests

---
+++ HubOfChess.Application/Messages/Queries/GetMessagesByChatId/GetMessagesByChatIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HubOfChess.Application.ViewModels;
+using MediatR;
+
+namespace HubOfChess.Application.Messages.Queries.GetMessagesByChatId
+{
+    public record GetMessagesByChatIdQuery(Guid ChatId, Guid UserId, int Page, int PageLimit) : IRequest<IEnumerable<MessageVM>>;
+}
