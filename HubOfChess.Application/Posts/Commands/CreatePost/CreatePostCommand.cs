211dcd6cf26130018bb014230af05b83e1c391b2
sanchez.101 <sanchez.101@mail.ru>
2022-05-07 18:32:45 +0300

    Add Create Post Command, Handler and tests

---
+++ HubOfChess.Application/Posts/Commands/CreatePost/CreatePostCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace HubOfChess.Application.Posts.Commands.CreatePost
+{
+    public record CreatePostCommand(Guid UserId, string Title, string Text) : IRequest<Guid>;
+}
---
+++ HubOfChess.Application/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, Guid>
+    {
+        private readonly IAppDbContext _dbContext;
+
+        public CreatePostCommandHandler(IAppDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.UserId == request.UserId);
+
+            if (user == null)
+                throw new NotFoundException(nameof(User), request.UserId);
+
+            var post = new Post
+            {
+                Id = Guid.NewGuid(),
+                Author = user,
+                Title = request.Title,
+                Text = request.Text,
+                Date = DateTime.Now
+            };
+
+            await _dbContext.Posts.AddAsync(post);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return post.Id;
+        }
+    }
+}
