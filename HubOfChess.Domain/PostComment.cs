3a5f4a1082e0138fd4f03dc5d2d31164c8987762
sanchez.101 <sanchez.101@mail.ru>
2022-05-08 21:24:00 +0300

    PostComment model refactoring & moved to separate file

---
+++ HubOfChess.Domain/PostComment.cs
@@ -0,0 +1,11 @@
+﻿namespace HubOfChess.Domain
+{
+    public class PostComment
+    {
+        public Guid Id { get; set; }
+        public Post Post { get; set; } = null!;
+        public User User { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public DateTime Date { get; set; }
+    }
+}
