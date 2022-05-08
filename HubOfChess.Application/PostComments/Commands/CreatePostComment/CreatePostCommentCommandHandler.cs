7a102a5d3fa22d54d2408f2f34625f5586781731
sanchez.101 <sanchez.101@mail.ru>
2022-05-08 16:52:43 +0300

    Add Create PostComment Command

---
+++ HubOfChess.Application/PostComments/Commands/CreatePostComment/CreatePostCommentCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Application.PostComments.Commands.CreatePostComment
+{
+    public class CreatePostCommentCommandHandler : IRequestHandler<CreatePostCommentCommand, Guid>
+    {
+        private readonly IAppDbContext _dbContext;
+
+        public CreatePostCommentCommandHandler(IAppDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreatePostCommentCommand request, CancellationToken cancellationToken)
+        {
+            var post = await _dbContext.Posts
+                .FirstOrDefaultAsync(p => p.Id == request.PostId);
+            var user = await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.UserId == request.UserId);
+
+            if (post == null)
+                throw new NotFoundException(nameof(Post), request.PostId);
+            if (user == null)
+                throw new NotFoundException(nameof(User), request.UserId);
+
+            var comment = new PostComment
+            {
+                Id = Guid.NewGuid(),
+                User = user,
+                Post = post,
+                Text = request.Text,
+                Date = DateTime.Now
+            };
+
+            await _dbContext.PostComments.AddAsync(comment);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return comment.Id;
+        }
+    }
+}
---
+++ HubOfChess.Tests/PostComments/Commands/CreatePostCommentCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using HubOfChess.Application.PostComments.Commands.CreatePostComment;
+using HubOfChess.Tests.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HubOfChess.Tests.PostComments.Commands
+{
+    public class CreatePostCommentCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task CreatePostCommentCommand_Success()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserA.UserId;
+            var postId = AppDbContextFactory.PostC.Id;
+            var text = "4FD8-A43F";
+            var handler = new CreatePostCommentCommandHandler(DbContext);
+
+            //Act
+            var commentId = await handler.Handle(
+                new CreatePostCommentCommand(userId, postId, text),
+                CancellationToken.None);
+            var comment = await DbContext.PostComments
+                .FirstOrDefaultAsync(c => c.Id == commentId);
+
+            //Assert
+            comment.Should().NotBeNull();
+            comment!.User.UserId.Should().Be(userId);
+            comment.Post.Id.Should().Be(postId);
+            comment.Text.Should().Be(text);
+        }
+    }
+}
