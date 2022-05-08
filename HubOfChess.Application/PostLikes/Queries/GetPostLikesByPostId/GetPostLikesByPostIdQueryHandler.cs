c57aa5159780406282eec1b2d4a3082e520a6342
sanchez.101 <sanchez.101@mail.ru>
2022-05-08 16:27:45 +0300

    Add GetPostLikesByPostId Query

---
+++ HubOfChess.Application/PostLikes/Queries/GetPostLikesByPostId/GetPostLikesByPostIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Application.ViewModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Application.PostLikes.Queries.GetPostLikesByPostId
+{
+    public class GetPostLikesByPostIdQueryHandler : IRequestHandler<GetPostLikesByPostIdQuery, IEnumerable<PostLikeVM>>
+    {
+        private readonly IAppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetPostLikesByPostIdQueryHandler(IAppDbContext dbContext, IMapper mapper) =>
+            (_dbContext, _mapper) = (dbContext, mapper);
+
+        public async Task<IEnumerable<PostLikeVM>> Handle(GetPostLikesByPostIdQuery request, CancellationToken cancellationToken)
+        {
+            var likesList = await _dbContext.PostLikes
+                .Where(l => l.PostId == request.PostId)
+                .ToListAsync();
+
+            return _mapper.Map <IEnumerable<PostLikeVM>>(likesList);
+        }
+    }
+}
---
+++ HubOfChess.Tests/PostLikes/Queries/GetPostLikesByPostIdQueryHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using HubOfChess.Application.PostLikes.Queries.GetPostLikesByPostId;
+using HubOfChess.Application.ViewModels;
+using HubOfChess.Tests.Common;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HubOfChess.Tests.PostLikes.Queries
+{
+    public class GetPostLikesByPostIdQueryHandlerTests : TestQueryBase
+    {
+        [Fact]
+        public async Task GetPostLikesByPostIdQuery_Success()
+        {
+            //Arrange
+            var postId = AppDbContextFactory.PostA.Id;
+            var handler = new GetPostLikesByPostIdQueryHandler(DbContext, Mapper);
+            var expectedList = Mapper.Map<IEnumerable<PostLikeVM>>( new[] {
+                AppDbContextFactory.PostLikeA, 
+                AppDbContextFactory.PostLikeB });
+
+            //Act
+            var result = await handler.Handle(
+                new GetPostLikesByPostIdQuery(postId),
+                CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().HaveCount(2);
+            result.Should().BeEquivalentTo(expectedList);
+        }
+
+        [Fact]
+        public async Task GetPostLikesByPostIdQuery_SuccessEmptyResult()
+        {
+            //Arrange
+            var postId = AppDbContextFactory.PostB.Id;
+            var handler = new GetPostLikesByPostIdQueryHandler(DbContext, Mapper);
+            var expectedList = Mapper.Map<IEnumerable<PostLikeVM>>(new[] {
+                AppDbContextFactory.PostLikeA,
+                AppDbContextFactory.PostLikeB });
+
+            //Act
+            var result = await handler.Handle(
+                new GetPostLikesByPostIdQuery(postId),
+                CancellationToken.None);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
