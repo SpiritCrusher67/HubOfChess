c7cdee376d50856048f50eb96fc238d2abc1a2cc
sanchez.101 <sanchez.101@mail.ru>
2022-05-08 17:10:30 +0300

    Add Delete PostComment Command

---
+++ HubOfChess.Application/PostComments/Commands/DeletePostComment/DeletePostCommentCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace HubOfChess.Application.PostComments.Commands.DeletePostComment
+{
+    public record DeletePostCommentCommand(Guid UserId, Guid CommentId) : IRequest;
+}
