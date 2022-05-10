6a015ba985bc71cc9a54843b43b0e9ef8c7a79f4
sanchez.101 <sanchez.101@mail.ru>
2022-05-10 16:54:01 +0300

    Add CreatePostCommand Validator

---
+++ HubOfChess.Application/Posts/Commands/CreatePost/CreatePostCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace HubOfChess.Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(cmd => cmd.UserId).NotNull().NotEqual(Guid.Empty);
+            RuleFor(cmd => cmd.Title).NotEmpty().MaximumLength(40);
+            RuleFor(cmd => cmd.Text).NotEmpty().MaximumLength(1000);
+        }
+    }
+}
