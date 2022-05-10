4b7622fc7dda16ac2d88e02e9aed33e3378b826b
sanchez.101 <sanchez.101@mail.ru>
2022-05-10 16:50:52 +0300

    Add CreateMessageCommand validator

---
+++ HubOfChess.Application/Messages/Commands/CreateMessage/CreateMessageCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace HubOfChess.Application.Messages.Commands.CreateMessage
+{
+    public class CreateMessageCommandValidator : AbstractValidator<CreateMessageCommand>
+    {
+        public CreateMessageCommandValidator()
+        {
+            RuleFor(cmd => cmd.ChatId).NotNull().NotEqual(Guid.Empty);
+            RuleFor(cmd => cmd.UserId).NotNull().NotEqual(Guid.Empty);
+            RuleFor(cmd => cmd.Text).NotEmpty().MaximumLength(200);
+        }
+    }
+}
