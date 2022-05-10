1a00523c1c7064153e227492b117e3e7f4b7b36e
sanchez.101 <sanchez.101@mail.ru>
2022-05-10 16:50:30 +0300

    Add UpdateChatCommand validator

---
+++ HubOfChess.Application/Chats/Commands/UpdateChat/UpdateChatCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace HubOfChess.Application.Chats.Commands.UpdateChat
+{
+    public class UpdateChatCommandValidator : AbstractValidator<UpdateChatCommand>
+    {
+        public UpdateChatCommandValidator()
+        {
+            RuleFor(cmd => cmd.ChatId).NotNull().NotEqual(Guid.Empty);
+            RuleFor(cmd => cmd.ChatName).MaximumLength(20);
+        }
+    }
+}
