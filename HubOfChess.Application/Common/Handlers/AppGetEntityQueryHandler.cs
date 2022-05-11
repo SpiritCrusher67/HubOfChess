d94b1a2fbd8e754b8264e9e7a6010a9140a413d4
sanchez.101 <sanchez.101@mail.ru>
2022-05-11 12:30:00 +0300

    Add IGetEntityQueryHandler<User> Implementation. Returns User or throws NotFoundException

---
+++ HubOfChess.Application/Common/Handlers/AppGetEntityQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Application.Common.Handlers
+{
+    public class AppGetEntityQueryHandler : 
+        IGetEntityQueryHandler<User>
+    {
+
+        private readonly IAppDbContext dbContext;
+
+        public AppGetEntityQueryHandler(IAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        async Task<User> IGetEntityQueryHandler<User>.GetEntityByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var user = await dbContext.Users
+                .FirstOrDefaultAsync(u => u.UserId == id, cancellationToken);
+            if (user == null)
+                throw new NotFoundException(nameof(User), id);
+            return user;
+        }
+    }
+}
