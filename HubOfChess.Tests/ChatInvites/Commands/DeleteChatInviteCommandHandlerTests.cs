7f3d5e977a5a692d1c4ccb83be6046b58dd70c51
sanchez.101 <sanchez.101@mail.ru>
2022-05-11 20:27:27 +0300

    Add Delete ChatInvite Command

---
+++ HubOfChess.Tests/ChatInvites/Commands/DeleteChatInviteCommandHandlerTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using HubOfChess.Tests.Common;
+using HubOfChess.Application.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using HubOfChess.Application.ChatInvites.Commands.DeleteChatInvite;
+using System;
+
+namespace HubOfChess.Tests.ChatInvites.Commands
+{
+    public class DeleteChatInviteCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task DeleteChatInviteCommand_SuccessBySenderUser()
+        {
+            //Arrange
+            var chatId = AppDbContextFactory.ChatInviteC.ChatId;
+            var userId = AppDbContextFactory.ChatInviteC.SenderUserId;
+            var handler = new DeleteChatInviteCommandHandler(DbContext);
+
+            //Act
+            await handler.Handle(
+                new DeleteChatInviteCommand(chatId, userId),
+                CancellationToken.None);
+            var invite = await DbContext.ChatInvites
+                .FirstOrDefaultAsync(i => i.ChatId == chatId && 
+                    i.InvitedUserId == AppDbContextFactory.ChatInviteC.InvitedUserId);
+
+            //Assert
+            invite.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task DeleteChatInviteCommand_SuccessByInvitedUser()
+        {
+            //Arrange
+            var chatId = AppDbContextFactory.ChatInviteC.ChatId;
+            var userId = AppDbContextFactory.ChatInviteC.InvitedUserId;
+            var handler = new DeleteChatInviteCommandHandler(DbContext);
+
+            //Act
+            await handler.Handle(
+                new DeleteChatInviteCommand(chatId, userId),
+                CancellationToken.None);
+            var invite = await DbContext.ChatInvites
+                .FirstOrDefaultAsync(i => i.ChatId == chatId && i.InvitedUserId == userId);
+
+            //Assert
+            invite.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CreateChatInviteCommand_FailOnNotWrongUserId()
+        {
+            //Arrange
+            var chatId = AppDbContextFactory.ChatInviteC.ChatId;
+            var userId = Guid.NewGuid();
+            var handler = new DeleteChatInviteCommandHandler(DbContext);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                handler.Handle(
+                    new DeleteChatInviteCommand(chatId,userId),
+                    CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task CreateChatInviteCommand_FailOnNotWrongChatId()
+        {
+            //Arrange
+            var chatId = Guid.NewGuid();
+            var userId = AppDbContextFactory.ChatInviteC.InvitedUserId;
+            var handler = new DeleteChatInviteCommandHandler(DbContext);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+                handler.Handle(
+                    new DeleteChatInviteCommand(chatId, userId),
+                    CancellationToken.None));
+        }
+    }
+}
