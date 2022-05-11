575726511bdcf0e88d33f44387ebb905f5a79235
sanchez.101 <sanchez.101@mail.ru>
2022-05-11 19:55:01 +0300

    Add Create ChatInvite Command

---
+++ HubOfChess.Application/ChatInvites/Commands/CreateChatInvite/CreateChatInviteCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Domain;
+using MediatR;
+
+namespace HubOfChess.Application.ChatInvites.Commands.CreateChatInvite
+{
+    public class CreateChatInviteCommandHandler : IRequestHandler<CreateChatInviteCommand>
+    {
+        private readonly IAppDbContext dbContext;
+        private readonly IGetEntityQueryHandler<User> getUserHandler;
+        private readonly IGetEntityQueryHandler<Chat> getChatHandler;
+
+        public CreateChatInviteCommandHandler(IAppDbContext dbContext, 
+            IGetEntityQueryHandler<User> getUserHandler,
+            IGetEntityQueryHandler<Chat> getChatHandler)
+        {
+            this.dbContext = dbContext;
+            this.getUserHandler = getUserHandler;
+            this.getChatHandler = getChatHandler;
+        }
+
+        public async Task<Unit> Handle(CreateChatInviteCommand request, CancellationToken cancellationToken)
+        {
+            var senderUser = await getUserHandler
+                .GetEntityByIdAsync(request.SenderUserId, cancellationToken);
+            var invitedUser = await getUserHandler
+                .GetEntityByIdAsync(request.InvitedUserId, cancellationToken);
+            var chat = await getChatHandler
+                .GetEntityByIdAsync(request.ChatId, cancellationToken);
+
+            if (senderUser != chat.Owner)
+                throw new NoPermissionException(
+                    nameof(User), senderUser.UserId,
+                    nameof(Chat), chat.Id);
+
+            var invite = new ChatInvite
+            {
+                Chat = chat,
+                SenderUser = senderUser,
+                InvitedUser = invitedUser,
+                InviteMessage = request.InviteMessage
+            };
+
+            await dbContext.ChatInvites.AddAsync(invite, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ HubOfChess.Tests/ChatInvites/Commands/CreateChatInviteCommandHandlerTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using HubOfChess.Tests.Common;
+using HubOfChess.Application.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using HubOfChess.Application.ChatInvites.Commands.CreateChatInvite;
+
+namespace HubOfChess.Tests.ChatInvites.Commands
+{
+    public class CreateChatInviteCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task CreateChatInviteCommand_Success()
+        {
+            //Arrange
+            var chatId = AppDbContextFactory.ChatB.Id;
+            var senderUserId = AppDbContextFactory.UserC.UserId;
+            var invitedUserId = AppDbContextFactory.UserA.UserId;
+            var inviteText = "B24F";
+            var handler = new CreateChatInviteCommandHandler(DbContext,QueryHandler, QueryHandler);
+
+            //Act
+            await handler.Handle(
+                new CreateChatInviteCommand(senderUserId, chatId,invitedUserId, inviteText),
+                CancellationToken.None);
+            var invite = await DbContext.ChatInvites
+                .FirstOrDefaultAsync(i => i.ChatId == chatId && i.InvitedUserId == invitedUserId);
+
+            //Assert
+            invite.Should().NotBeNull();
+            invite!.SenderUserId.Should().Be(senderUserId);
+            invite.InviteMessage.Should().Be(inviteText);
+        }
+
+        [Fact]
+        public async Task CreateChatInviteCommand_FailOnNotOwnerUser()
+        {
+            //Arrange
+            var chatId = AppDbContextFactory.ChatB.Id;
+            var senderUserId = AppDbContextFactory.UserB.UserId;
+            var invitedUserId = AppDbContextFactory.UserA.UserId;
+            var inviteText = "B24F";
+            var handler = new CreateChatInviteCommandHandler(DbContext, QueryHandler, QueryHandler);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NoPermissionException>(() =>
+                handler.Handle(
+                    new CreateChatInviteCommand(senderUserId, chatId, invitedUserId, inviteText),
+                    CancellationToken.None));
+        }
+    }
+}
