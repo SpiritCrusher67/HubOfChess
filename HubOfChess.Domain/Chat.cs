71b350b0ddac9bf6eda930c909e0500a66d9de1f
sanchez.101 <sanchez.101@mail.ru>
2022-05-11 14:21:18 +0300

    Chat model refactoring

--- HubOfChess.Domain/Chat.cs
+++ HubOfChess.Domain/Chat.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string? Name { get; set; }
-        public User? Owner { get; set; }
+        public User Owner { get; set; } = null!;
         public ICollection<User> Users { get; set; } = null!;
         public ICollection<Message> Messages { get; set; } = null!;
     }
