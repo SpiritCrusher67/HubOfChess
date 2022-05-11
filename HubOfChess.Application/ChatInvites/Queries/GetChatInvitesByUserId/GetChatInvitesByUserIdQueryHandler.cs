878df7e579e2fb31aed2810b70654cca28e0a776
sanchez.101 <sanchez.101@mail.ru>
2022-05-11 21:50:58 +0300

    Add GetChatInvite ByUserId Query

---
+++ HubOfChess.Application/ChatInvites/Queries/GetChatInvitesByUserId/GetChatInvitesByUserIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Application.ViewModels;
+using HubOfChess.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HubOfChess.Application.ChatInvites.Queries.GetChatInvitesByUserId
+{
+    public class GetChatInvitesByUserIdQueryHandler : IRequestHandler<GetChatInvitesByUserIdQuery, IEnumerable<ChatInviteVM>>
+    {
+        private readonly IAppDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public GetChatInvitesByUserIdQueryHandler(IAppDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ChatInviteVM>> Handle(GetChatInvitesByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await dbContext.Users
+                .Include(u => u.ChatInvites)
+                .FirstOrDefaultAsync(u => u.UserId == request.UserId);
+            if (user == null)
+                throw new NotFoundException(nameof(User), request.UserId);
+
+            return mapper.Map<IEnumerable<ChatInviteVM>>(user.ChatInvites);
+        }
+    }
+}
---
+++ HubOfChess.Tests/ChatInvites/Queries/GetChatInvitesByUserIdQueryHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HubOfChess.Application.ChatInvites.Queries.GetChatInvitesByUserId;
+using HubOfChess.Application.Common.Exceptions;
+using HubOfChess.Application.ViewModels;
+using HubOfChess.Domain;
+using HubOfChess.Tests.Common;
+using Xunit;
+
+namespace HubOfChess.Tests.ChatInvites.Queries
+{
+    public class GetChatInvitesByUserIdQueryHandlerTests : TestQueryBase
+    {
+        [Fact]
+        public async Task GetChatInvitesByUserIdQuery_Success()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserC.UserId;
+            var handler = new GetChatInvitesByUserIdQueryHandler(DbContext, Mapper);
+            var expectedInvites = Mapper.Map<IEnumerable<ChatInviteVM>>(new List<ChatInvite>
+            {
+                AppDbContextFactory.ChatInviteB,
+                AppDbContextFactory.ChatInviteC
+            });
+
+            //Act
+            var result = await handler.Handle(
+                new GetChatInvitesByUserIdQuery(userId),
+                CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(expectedInvites);
+        }
+
+        [Fact]
+        public async Task GetChatInvitesByUserIdQueryt_SuccessEmptyResult()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserA.UserId;
+            var handler = new GetChatInvitesByUserIdQueryHandler(DbContext, Mapper);
+
+            //Act
+            var result = await handler.Handle(
+                new GetChatInvitesByUserIdQuery(userId),
+                CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetChatInvitesByUserIdQueryt_FailOnWrongUserId()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var handler = new GetChatInvitesByUserIdQueryHandler(DbContext, Mapper);
+
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<NotFoundException>(() =>
+               handler.Handle(
+               new GetChatInvitesByUserIdQuery(userId),
+               CancellationToken.None));
+        }
+    }
+}
