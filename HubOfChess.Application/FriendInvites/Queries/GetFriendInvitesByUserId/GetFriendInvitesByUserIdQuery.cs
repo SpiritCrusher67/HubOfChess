43de0eb747cf567de9d7e7fc61068a638efc16da
sanchez.101 <sanchez.101@mail.ru>
2022-05-12 18:54:19 +0300

    Add GetFriendInvites ByUserId Query

---
+++ HubOfChess.Application/FriendInvites/Queries/GetFriendInvitesByUserId/GetFriendInvitesByUserIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using HubOfChess.Application.ViewModels;
+using MediatR;
+
+namespace HubOfChess.Application.FriendInvites.Queries.GetFriendInvitesByUserId
+{
+    public record GetFriendInvitesByUserIdQuery(Guid UserId) : IRequest<IEnumerable<FriendInviteVM>>;
+}
---
+++ HubOfChess.Tests/FriendInvites/Queries/GetFriendInvitesByUserIdQueryHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HubOfChess.Application.FriendInvites.Queries.GetFriendInvitesByUserId;
+using HubOfChess.Application.ViewModels;
+using HubOfChess.Domain;
+using HubOfChess.Tests.Common;
+using Xunit;
+
+namespace HubOfChess.Tests.FriendInvites.Queries
+{
+    public class GetFriendInvitesByUserIdQueryHandlerTests : TestQueryBase
+    {
+        [Fact]
+        public async Task GetFriendInvitesByUserIdQuery_Success()
+        {
+            //Arrange
+            var userId = AppDbContextFactory.UserB.UserId;
+            var handler = new GetFriendInvitesByUserIdQueryHandler(QueryHandler, Mapper);
+            var expectedInvites = Mapper.Map<IEnumerable<FriendInviteVM>>(new List<FriendInvite>
+            {
+                AppDbContextFactory.FriendInviteA,
+                AppDbContextFactory.FriendInviteB
+            });
+
+            //Act
+            var result = await handler.Handle(
+                new GetFriendInvitesByUserIdQuery(userId),
+                CancellationToken.None);
+
+            //Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(expectedInvites);
+        }
+    }
+}
