c9d5051fff56e6030886f7a0eb2797d54ec579b5
sanchez.101 <sanchez.101@mail.ru>
2022-05-12 21:13:07 +0300

    Add DI config from Persistece layer

---
+++ HubOfChess.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using HubOfChess.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HubOfChess.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite();
+            });
+
+            services.AddScoped<IAppDbContext>(provider => 
+                provider.GetService<AppDbContext>()!);
+
+            return services;
+        }
+    }
+}
