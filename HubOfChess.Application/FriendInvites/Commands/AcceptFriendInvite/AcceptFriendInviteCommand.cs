28596928413d7eaf840d7165a1789ab3dbd0266d
sanchez.101 <sanchez.101@mail.ru>
2022-05-12 18:31:21 +0300

    Add AcceptFriendInvite Command

---
+++ HubOfChess.Application/FriendInvites/Commands/AcceptFriendInvite/AcceptFriendInviteCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace HubOfChess.Application.FriendInvites.Commands.AcceptFriendInvite
+{
+    public record AcceptFriendInviteCommand(Guid UserId, Guid SederId) : IRequest;
+}
