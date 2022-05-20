0090ecfdedca63e9de3dbdaef863922516520ec3
sanchez.101 <sanchez.101@mail.ru>
2022-05-20 14:51:55 +0300

    Add CreateChat Dto

---
+++ HubOfChess.WebApi/Models/Chat/CreateChatDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HubOfChess.Application.Chats.Commands.CreateChat;
+
+namespace HubOfChess.WebApi.Models.Chat
+{
+    public class CreateChatDto : BaseDtoModel<CreateChatCommand>
+    {
+        public string? ChatName { get; set; }
+
+        public override void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateChatDto, CreateChatCommand>()
+                .ForMember(command => command.ChatOwnerUserId,
+                    opt => opt.MapFrom(dto => dto.userId))
+                .ForMember(command => command.ChatName,
+                    opt => opt.MapFrom(dto => dto.ChatName));
+        }
+    }
+}
