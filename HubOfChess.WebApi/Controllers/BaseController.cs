e19031fbbdf06a1bd2cea6854fff7445d5a62c95
sanchez.101 <sanchez.101@mail.ru>
2022-05-20 14:52:27 +0300

    Add base Api controller

---
+++ HubOfChess.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HubOfChess.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+        protected IMediator Mediator { get; }
+        protected Guid UserId => User?.Identity?.IsAuthenticated == true 
+            ? Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value) 
+            : Guid.Empty;
+
+        protected BaseController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+}
