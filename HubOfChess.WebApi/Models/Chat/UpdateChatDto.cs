198027d0917cf1e2598971d0744f2e76e380fb1b
sanchez.101 <sanchez.101@mail.ru>
2022-05-20 14:52:11 +0300

    Add UpdateChat Dto

---
+++ HubOfChess.WebApi/Models/Chat/UpdateChatDto.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HubOfChess.Application.Chats.Commands.UpdateChat;
+
+namespace HubOfChess.WebApi.Models.Chat
+{
+    public class UpdateChatDto : BaseDtoModel<UpdateChatCommand>
+    {
+        public Guid ChatId { get; set; }
+        public string? ChatName { get; set; }
+        public Guid? NewChatOwnerId { get; set; }
+
+        public override void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateChatDto, UpdateChatCommand>()
+                .ForMember(command => command.UserId,
+                    opt => opt.MapFrom(dto => dto.userId))
+                .ForMember(command => command.ChatName,
+                    opt => opt.MapFrom(dto => dto.ChatName))
+                .ForMember(command => command.ChatOwnerId,
+                    opt => opt.MapFrom(dto => dto.NewChatOwnerId));
+        }
+    }
+}
