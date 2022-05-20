931118279181d610bdd53c4c000bc471a9e70b21
sanchez.101 <sanchez.101@mail.ru>
2022-05-20 13:03:21 +0300

    Add services: AutoMapper, Application and Pericstence layers. Db initialize. Routing

--- HubOfChess.WebApi/Program.cs
+++ HubOfChess.WebApi/Program.cs
@@ -1,6 +1,45 @@
+using HubOfChess.Application;
+using HubOfChess.Application.Common.Mappings;
+using HubOfChess.Application.Interfaces;
+using HubOfChess.Persistence;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IAppDbContext).Assembly));
+});
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddCors(); //TODO: Configure cors
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AppDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        //TODO: use exception handler
+    }
+}
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
