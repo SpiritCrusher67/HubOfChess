6875186a6c6befe15a7d9359d326620b13d49b93
sanchez.101 <sanchez.101@mail.ru>
2022-05-21 15:16:11 +0300

    Add CreatePost Dto

---
+++ HubOfChess.WebApi/Models/Post/CreatePostDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HubOfChess.Application.Posts.Commands.CreatePost;
+
+namespace HubOfChess.WebApi.Models.Post
+{
+    public class CreatePostDto : BaseDtoModel<CreatePostCommand>
+    {
+        public string Title { get; set; } = null!;
+        public string Text { get; set; } = null!;
+
+        public override void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreatePostDto, CreatePostCommand>()
+                .ForMember(command => command.UserId,
+                    opt => opt.MapFrom(dto => dto.userId))
+                .ForMember(command => command.Title,
+                    opt => opt.MapFrom(dto => dto.Title))
+                .ForMember(command => command.Text,
+                    opt => opt.MapFrom(dto => dto.Text));
+        }
+    }
+}
