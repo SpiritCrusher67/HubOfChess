9f3833b10b6d8333a6b1bef568d9bc663183bba6
sanchez.101 <sanchez.101@mail.ru>
2022-05-21 17:34:22 +0300

    Add Create Chat Invite Dto

---
+++ HubOfChess.WebApi/Models/Chat/CreateChatInviteDto.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HubOfChess.Application.ChatInvites.Commands.CreateChatInvite;
+
+namespace HubOfChess.WebApi.Models.Chat
+{
+    public class CreateChatInviteDto : BaseDtoModel<CreateChatInviteCommand>
+    {
+        public Guid ChatId { get; set; }
+        public Guid InvitedUserId { get; set; }
+        public string? Message { get; set; }
+
+        public override void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateChatInviteDto, CreateChatInviteCommand>()
+                .ForMember(command => command.SenderUserId,
+                    opt => opt.MapFrom(dto => dto.userId))
+                .ForMember(command => command.InvitedUserId,
+                    opt => opt.MapFrom(dto => dto.InvitedUserId))
+                .ForMember(command => command.InviteMessage,
+                    opt => opt.MapFrom(dto => dto.Message));
+        }
+    }
+}
