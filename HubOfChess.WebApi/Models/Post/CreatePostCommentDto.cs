080bfca7832f49d39d232ef434599abe67d92cb1
sanchez.101 <sanchez.101@mail.ru>
2022-05-21 16:55:07 +0300

    Add post comment dto

---
+++ HubOfChess.WebApi/Models/Post/CreatePostCommentDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HubOfChess.Application.PostComments.Commands.CreatePostComment;
+
+namespace HubOfChess.WebApi.Models.Post
+{
+    public class CreatePostCommentDto : BaseDtoModel<CreatePostCommentCommand>
+    {
+        public Guid PostId { get; set; }
+        public string Text { get; set; } = null!;
+
+        public override void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreatePostCommentDto, CreatePostCommentCommand>()
+                .ForMember(command => command.UserId,
+                    opt => opt.MapFrom(dto => dto.userId))
+                .ForMember(command => command.PostId,
+                    opt => opt.MapFrom(dto => dto.PostId))
+                .ForMember(command => command.Text,
+                    opt => opt.MapFrom(dto => dto.Text));
+        }
+    }
+}
